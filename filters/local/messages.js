7a6997aa70fb4fc44547b33e425dc52d2f4d7861
Christian Rémillard <christian.remillard@gmail.com>
2022-12-15 15:04:42 -0500

    Ajout d'un message pour le filtre friendly-url

--- filters/local/messages.js
+++ filters/local/messages.js
@@ -44,10 +44,6 @@ export default {
         msg: "Ce lien pointe vers un guide hébergé sur LibGuides (<code>https://libguides.bib.umontreal.ca/c.php?...</code>). Utilisez plutôt une adresse pointant vers le guide sur le site Web des bibliothèques (<code>https://bib.umontreal.ca/...</code>)"
     },
 
-    // "bib-lien-libelle": {
-    //     msg: "Il est préférable d'utiliser des mots significatifs (par exemple le titre de la page) plutôt que l'adresse URL comme texte cliquable. Exception fréquentes : lorsque l'URL est citée dans une référence bibliographique; ou encore lorsqu'on on cite une adresse simplifiée. <span class=\"texte-petit\">( <a href=\"http://mentor.bib.umontreal.ca/comite-web/bpub/outils/Contribute/Aide-m%C3%A9moire%20Contribute.docx\" target=\"_blank\">plus d'infos</a>) </span>. "
-    // },
-
     "bib-Maestro": {
         "code": "bib-Maestro",
         "msg": "Ce lien pointe vers Maestro. Veuillez utiliser la nouvelle liste de bases de données de A à Z (<a href=\"https://libguides.bib.umontreal.ca/az.php\" target=\"_blank\"><code>https://libguides.bib.umontreal.ca/az.php</code></a>)"
@@ -59,6 +55,10 @@ export default {
 
     "bib-retrait-di": {
         msg: "Le site de la Didacthèque a été retiré. Veuillez corriger le lien."
+    },
+
+    "friendly-url": {
+        msg: "Cette ressource n'a pas de 'Friendly URL'. L'URL inscrite dans  la liste A à Z de LibGuides doit débuter par https://libguides.bib.umontreal.ca/"
     },
 
     "pup-timeout-redirect": {
